19df374ca5a651b661c0beb55b77ad4fea55f4fd
okatsn <okatsn@gmail.com>
2023-04-11 17:21:09 +0800

    Close this branch

--- src/TWAISWCF.jl
+++ src/TWAISWCF.jl
@@ -7,15 +7,4 @@ using SWCForecastBase
 export PrepareTable, PrepareTableDefault, preparetable!, ConfigAccumulate, ConfigPreprocess, ConfigSeriesToSupervised
 export traintest!, train!, test!, save
 
-using CairoMakie, OkMakieToolkits
-PredictData(PT::PredictTable, i) = PredictData(
-    PT.cache.test.args.Y_pred,
-    PT.cache.test.args.Y_real,
-    i)
-
-export PredictData, timeseriesplot, timeseriesplot!
-# import SWCForecastBase: TrainTestState
-# export TrainTestState
-
-
 end
