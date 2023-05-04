1fe2358a572d98e3b5040bc036845a58d57c7a0b
okatsn <okatsn@gmail.com>
2023-05-04 16:26:37 +0800

    Scirpts, docs env

--- scripts/demo_01.jl
+++ scripts/demo_01.jl
@@ -1,9 +1,16 @@
-using Revise, TWAISWCF
+using Revise, TWAISWCF, SWCExampleDatasets
 df = SWCExampleDatasets.dataset("NCUWiseLab", "ARI_G2F820_example")
 
+using DataFrames
 PT = PrepareTableDefault(df)
+PT2 = PrepareTable(df,
+    ConfigPreprocess(;target_features=Cols(r"soil_water_content_10cm")),
+    ConfigAccumulate(; intervals = [24, 48, 36, 72]),
+    ConfigSeriesToSupervised(; shift_x=[0, -2])
+)
+traintest!(PT2; train_before=DateTime(2022, 03, 21), test_after=DateTime(2022, 3, 22))
 
-traintest!(PT; train_before = DateTime(2022, 03, 21), test_after = DateTime(2022, 3, 22))
+traintest!(PT; train_before=DateTime(2022, 03, 21), test_after=DateTime(2022, 3, 22))
 
 # # Or alternatively
 # ```julia
